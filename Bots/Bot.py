9e2a6830a483f5b35e32bc82a8c44446af533643
marianadick <dickmariana1@gmail.com>
2021-11-29 15:44:15 -0300

    classe abstrata bots

--- Bots/Bot.py
+++ Bots/Bot.py
@@ -5,20 +5,28 @@ import random as r
 
 class Bot(ABC):
 
-    def __init__(nome,):
-        self.nome = nome
-        self.comandos = {}
+    def __init__(self, nome):
+        self.__nome = nome
+        self.__comandos = {}
 
-    #nao esquecer o decorator
+    @property
     def nome(self):
-        pass
+        return self.__nome
 
-    #nao esquecer o decorator
-    def nome(nome):
-        pass
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def comandos(self):
+        return self.__comandos
 
     def mostra_comandos(self):
-        pass
+        count = 0
+        for comando in self.comandos:
+            count += 1
+            print(f'{count} - {comando}')
+
 
     @abstractmethod
     def executa_comando(self,cmd):
