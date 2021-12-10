288629b0311d80288973fadf2aaded06aea52285
marianadick <dickmariana1@gmail.com>
2021-12-09 21:26:23 -0300

    adequações no bot marombeiro e algumas remoções

--- Bots/BotMarombeiro.py
+++ Bots/BotMarombeiro.py
@@ -1,15 +1,13 @@
-#grupo 3
-
 from Bots.Bot import Bot
-
+from Bots.Comando import Comando
 class BotMarombeiro(Bot):
     def __init__(self, nome: str):
         self.__nome = nome
-        self.__comandos = {
-            "Eai frango": "Frango é tu rapaz, tem nem 40 de braço, ta doido!?!",
-            "Me passa um treino?": "3x10 supino\n3x10 barra fixa\n3x10 rosca direta\n3x10 tríceps testa\n(perna não precisa)",
-            "Conselho": "Quer ficar grande? Tem que comer e treinar todo dia!"
-        }
+        self.__comandos = [
+            Comando('Eai frango', 'Frango é tu rapaz, tem nem 40 de braço, ta doido!?!'),
+            Comando('Me passa um treino?', '3x10 supino\n3x10 barra fixa\n3x10 rosca direta\n3x10 tríceps testa\n(perna não precisa)'),
+            Comando('Conselho', 'Quer ficar grande? Tem que comer e treinar todo dia!')]
+        self.__mensagem_de_erro = 'Digita o comando certo, seu frango!'
 
     @property
     def nome(self):
@@ -19,20 +17,23 @@ class BotMarombeiro(Bot):
     def nome(self, nome):
         self.__nome = nome
 
-    def apresentacao(self):
-        return "Treino e dieta eu não furo, tá ligado?"
- 
-    def mostra_comandos(self):
+    @property
+    def comandos(self):
         return self.__comandos
-    
-    def executa_comando(self,cmd):
-        try:
-            return self.__comandos[cmd]
-        except:
-            print("Ah isso eu não sei...")
+
+    @property
+    def mensagem_de_erro(self):
+        return self.__mensagem_de_erro
+
+    @mensagem_de_erro.setter
+    def mensagem_de_erro(self, mensagem_de_erro):
+        self.__mensagem_de_erro = mensagem_de_erro
+
+    def apresentacao(self):
+        return 'Treino e dieta eu não furo, tá ligado?'
 
     def boas_vindas(self):
-        return "HORA DO SHOW P****!"
+        return 'HORA DO SHOW P****!'
 
     def despedida(self):
-        return "Valeu mermão, até a próxima."
+        return 'Valeu mermão, até a próxima.'
--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from Bots.BotManezinho import BotManezinho
 from Bots.BotMinerin import BotMinerin
 from Bots.BotEspelhado import BotEspelhado
 from Bots.BotGamer import BotGamer
+from Bots.BotMarombeiro import BotMarombeiro
 from SistemaChatBot import SistemaChatBot as scb
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotMinerin("João"), BotManezinho('Ixtepô'), BotEspelhado('ohnidahlepsE'), BotGamer('Coldzera')]
+lista_bots = [BotMinerin("João"), BotManezinho('Ixtepô'), BotEspelhado('ohnidahlepsE'), 
+              BotGamer('Coldzera'), BotMarombeiro('Stronda')]
 
 sys = scb.SistemaChatBot("GoBots", lista_bots)
 sys.inicio()
